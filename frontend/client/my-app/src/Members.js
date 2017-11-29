c980578526e9cae7af4819d2e895d1c580dc9077
ruleroy <rovannakittikun@gmail.com>
2017-11-28 19:41:08 -0800

    video table created dynamically
    TODO: Video table pulls from database

--- frontend/client/my-app/src/Members.js
+++ frontend/client/my-app/src/Members.js
@@ -3,20 +3,61 @@ import logo from './huevosranchero.png';
 import './css/Members.css';
 import { Alert, Form, FormControl, Button, Panel, Table, Grid, Row, Col} from 'react-bootstrap';
 import { connect } from 'react-redux';
+import VideoTable from './VideoTable';
+
 const request = require('superagent');
+var currentTime = new Date().toString().split(' ').splice(1,4).join(' ');
 
 class Members extends Component {
 	constructor(props){
 		super(props);
 
 		this.state = {
-			inputValue : 'Only .mp4 files allowed.'
+			inputValue : 'Only .mp4 files allowed.',
+			data: [{
+				id: 1,
+				name: "test",
+				date: currentTime
+			}, {
+				id: 2,
+				name: "test2",
+				date: currentTime
+			}, {
+				id: 3,
+				name: "test3",
+				date: currentTime
+			}],
+			rows: [],
+			fields: ['id', 'name', 'date']
 		}
+
+		this.video = this.video.bind(this);
+	}
+
+	componentWillMount(){
+		this.video();
+	}
+
+	video(){
+		//console.log("HEYY");
+		var that = this;
+		let fields = ['id', 'name', 'date'];
+		this.state.data.forEach(function(data){
+			that.state.rows.push(
+				<VideoTable key={data.id} 
+				tbodyIdKey={data.id} 
+				rowData={data} 
+				dataOrder={fields} 
+				/>);
+		});
+		console.log(this.state.rows);
 	}
 
 	logout(){
+		
 		this.props.dispatch({type: 'LOG_OUT'});
 		this.props.history.push("/");
+		
 	}
 
 	browse(){
@@ -40,11 +81,6 @@ class Members extends Component {
 
 	}
 
-	handleSubmit(event) {
-		alert('A name was submitted: ' + this.state.value);
-		event.preventDefault();
-	}
-
 	upload = e => {
 		console.log(this.fileName.files[0]);
 		e.preventDefault();
@@ -56,100 +92,106 @@ class Members extends Component {
 		.end(function(err, res){
 			if(err){
 				alert(err);
-				return;
-			}
-			alert("File uploaded successfully!");
-		});
-
-		//var newform = new FormData(document.getElementById("formUpload"));
-
-/*
-		fetch('http://localhost:3001/api/upload', {
-			method: 'POST',
-			body: newform
-		});
-		*/
-		
+		return;
 	}
+	alert("File uploaded successfully!");
+});
 
-	render(){
-		return (
+}
+
+render(){
+	return (
+		<div>
+		<div className="headingBlock">
+		<div className="logoutDiv">
+		<Button bsStyle="danger" className="logout" onClick={this.logout.bind(this)}>Sign out</Button>
+		</div>
+		<img src={logo} className="logo" alt="huevos_ranchero"/>
+		<h3 className="center">Welcome {this.props.username}!</h3>
+		</div>
+		<Panel className="panel innerBlock" bsStyle="primary" header={
 			<div>
-			<div className="headingBlock">
-			<div className="logoutDiv">
-			<Button bsStyle="danger" className="logout" onClick={this.logout.bind(this)}>Sign out</Button>
+			<h4>Videos</h4>
 			</div>
-			<img src={logo} className="logo" alt="huevos_ranchero"/>
-			<h3 className="center">Welcome {this.props.username}!</h3>
-			</div>
-			<Panel className="panel innerBlock" bsStyle="primary" header={
-				<div>
-				<h4>Videos</h4>
-				</div>
-			}>
-
-			
-			<Form onSubmit={this.upload.bind(this)} encType='multipart/form-data' ref='uploadForm' id='uploadForm' method='POST' action='http://localhost:3001/api/upload'>
-			<input type="hidden" name="username" value={this.props.username} />
+		}>
 
 
-			<div className="uploadBlock">
-			<div className="input-group">
-			<label className="input-group-btn">
-			<span className="btn btn-primary">
-			Browse... 
-			<FormControl 
-			name="videoFile"
-			inputRef={input => this.fileName = input} 
-			type="file"
-			accept="video/mp4"
-			onChange={this.browse.bind(this)} 
-			style={displayNone}/>
-			</span>
-			</label>
-			<input type="text" className="form-control" value={this.state.inputValue} disabled />
-			</div>
-			</div>
-
-			<Button className="upload" bsStyle="primary" type="submit">Upload video</Button>
-			</Form>
-
-			
-			<br/>
-			<br/>
-			<br/>
-			<Table bordered condensed hover>
-			<thead>
-			<tr>
-			<th>#</th>
-			<th>Name</th>
-			<th>Date</th>
-			</tr>
-			</thead>
-			<tbody>
-			<tr>
-			<td>1</td>
-			<td>Video 1</td>
-			<td>test</td>
-			</tr>
-			<tr>
-			<td>2</td>
-			<td>Video 2</td>
-			<td>test2</td>
-			</tr>
-			<tr>
-			<td>3</td>
-			<td>Video 3</td>
-			<td>test3</td>
-			</tr>
-			</tbody>
-			</Table>
+		<Form onSubmit={this.upload.bind(this)} encType='multipart/form-data' ref='uploadForm' id='uploadForm' method='POST' action='http://localhost:3001/api/upload'>
+		<input type="hidden" name="username" value={this.props.username} />
 
 
-			</Panel>
-			</div>
-			);
-	}
+		<div className="uploadBlock">
+		<div className="input-group">
+		<label className="input-group-btn">
+		<span className="btn btn-primary">
+		Browse... 
+		<FormControl 
+		name="videoFile"
+		inputRef={input => this.fileName = input} 
+		type="file"
+		accept="video/mp4"
+		onChange={this.browse.bind(this)} 
+		style={displayNone}/>
+		</span>
+		</label>
+		<input type="text" className="form-control" value={this.state.inputValue} disabled />
+		</div>
+		</div>
+
+		<Button className="upload" bsStyle="primary" type="submit">Upload video</Button>
+		</Form>
+
+
+		<br/>
+		<br/>
+		<br/>
+
+		<Table bordered condensed hover>
+		<thead>
+		<tr>
+		<th>#</th>
+		<th>Name</th>
+		<th>Date</th>
+		</tr>
+		</thead>
+
+		<tbody>
+		{this.state.rows}
+		</tbody>
+		</Table>
+
+		<Table bordered condensed hover>
+		<thead>
+		<tr>
+		<th>#</th>
+		<th>Name</th>
+		<th>Date</th>
+		</tr>
+		</thead>
+		<tbody>
+		<tr>
+		<td>1</td>
+		<td>Video 1</td>
+		<td>test</td>
+		</tr>
+		<tr>
+		<td>2</td>
+		<td>Video 2</td>
+		<td>test2</td>
+		</tr>
+		<tr>
+		<td>3</td>
+		<td>Video 3</td>
+		<td>test3</td>
+		</tr>
+		</tbody>
+		</Table>
+
+
+		</Panel>
+		</div>
+		);
+}
 }
 
 const displayNone = {
---
+++ frontend/client/my-app/src/VideoTable.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class VideoTable extends Component{
+	constructor(props){
+		super(props);
+		this.createRow = this.createRow.bind(this);
+	}
+
+	createRow(tableRowID, data, dataOrder, cells = []){
+		for(var i in dataOrder){
+			cells.push(<td key={i}>{data[dataOrder[i]]}</td>);
+		}
+
+		return(<tr key={i}>{cells}</tr>);
+	}
+
+	render(){
+		return this.createRow(
+			this.props.tbodyIdKey,
+			this.props.rowData,
+			this.props.dataOrder
+			);
+	}
+
+	/*this.createRow(
+			this.props.tbodyIdKey,
+			this.props.rowData,
+			this.props.dataOrder
+			);*/
+}
+
+export default VideoTable;
