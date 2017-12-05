7d55e441634a51b64e1f3a773ae8e1a93d8d98cc
Christopher <christopher.dalporto@outlook.com>
2017-12-05 03:06:12 -0800

    Delete unused files

--- frontend/registration.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-New User Registration <br>
-Name: <?php echo $_POST["name"]; ?><br>
-Username: <?php echo $_POST["user"]; ?><br>
-Password: <?php echo $_POST["pass"]; ?><br>
-
-</body>
-</html>
